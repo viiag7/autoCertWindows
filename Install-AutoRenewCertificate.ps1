1c9ffd224f752ba83fd5628f615a5634ce2fa79e
viiag7 <vinicius@vi7k.com.br>
2024-08-30 11:30:14 -0300

    assinatura de script

--- Install-AutoRenewCertificate.ps1
+++ Install-AutoRenewCertificate.ps1
@@ -6,9 +6,9 @@
 
 .AUTHOR Vinicius Aguiar
 
-.COMPANYNAME Vinicius Aguiar Solutions
+.COMPANYNAME vi7k.com.br
 
-.COPYRIGHT (c) 2024 Vinicius Aguiar. Todos os direitos reservados.
+.COPYRIGHT
 
 .TAGS SSL, Certificado, Automação, PowerShell, Agendamento
 
@@ -22,18 +22,13 @@
 
 .REQUIREDSCRIPTS
 
-.EXTERNALSCRIPTDEPENDENCIES Nenhum
+.EXTERNALSCRIPTDEPENDENCIES
 
 .RELEASENOTES
-## Versão 1.0.0
-- Primeira versão do script para automação de renovação de certificados SSL.
-
-.PRIVATEDATA
-
+Versão 1.0.0:   Primeira versão do script para automação de renovação de certificados SSL.
 #>
 
 <#
-
 .DESCRIPTION
  Script para automatizar a renovação de certificados SSL usando agendamento de tarefas no Windows.
 
@@ -75,11 +70,11 @@
     O ID do locatário (Tenant ID) do Azure.
 
 .EXAMPLE
-    .\install-autorenew.ps1
+    .\Install-AutoRenewCertificate.ps1
     Este exemplo executa o script e solicita as entradas necessárias para configurar a tarefa agendada.
 
 .NOTES
-    Nome: install-autorenew.ps1
+    Nome: Install-AutoRenewCertificate.ps1
     Versão: 1.0.0
     Autor: Vinicius Aguiar
     Data: 30/08/2024
@@ -166,3 +161,36 @@ $azureVariablesContent = @"
 Set-Content -Path $azureVariablesPath -Value $azureVariablesContent
 
 Write-Host "Os atributos do Azure foram salvos em $azureVariablesPath"
+
+# SIG # Begin signature block
+# MIIFmAYJKoZIhvcNAQcCoIIFiTCCBYUCAQExDzANBglghkgBZQMEAgEFADB5Bgor
+# BgEEAYI3AgEEoGswaTA0BgorBgEEAYI3AgEeMCYCAwEAAAQQH8w7YFlLCE63JNLG
+# KX7zUQIBAAIBAAIBAAIBAAIBADAxMA0GCWCGSAFlAwQCAQUABCDr1qgkTAbb5r/d
+# 3TQnLmz4CWHmvpQa3QmMkiP+4CO1g6CCAwwwggMIMIIB8KADAgECAhBWuR3GMKEN
+# tk9swcznHdg7MA0GCSqGSIb3DQEBCwUAMBwxGjAYBgNVBAMMEU15Q29kZVNpZ25p
+# bmdDZXJ0MB4XDTI0MDgzMDE0MTYxN1oXDTI1MDgzMDE0MzYxN1owHDEaMBgGA1UE
+# AwwRTXlDb2RlU2lnbmluZ0NlcnQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK
+# AoIBAQDXYdv2bUdJDWxVTAJw5SqfSfHAfOPsC/ZOmMcDxEFav51bSPx2oVTfH/F+
+# lSU3DnCncfXejs3rU+RKyBw80GgD9m1ZPZ+QOEJE+8sxi2d2OI8UDrKhBVqpQK8I
+# yJ7VwET4yURe/KBEL+H16Ye6pGfTyIOmKyRRNwkEHKLNquE1lTgEtexc6NDdDWNZ
+# ZtwIr7TfJYHL7CVWqnULzTFwIqN5EQGSjsVjhxlOUcU91OOm2x9S852EynE/phoR
+# xHtV1NCAJhNB9/uSc5yTcsCpEsSInjeCiNuOM0gF7gsaX7nsiHbbhGf9/wfzGJRJ
+# pxJ2cN/wUvxzUvfBPi16lldLiVHVAgMBAAGjRjBEMA4GA1UdDwEB/wQEAwIHgDAT
+# BgNVHSUEDDAKBggrBgEFBQcDAzAdBgNVHQ4EFgQUwQqoeAcyiQ8pg2gbc0P2kgMM
+# IN4wDQYJKoZIhvcNAQELBQADggEBAHenXTWvOKcriSXMfAHZwiw5wdT6SUuTSKYJ
+# a55dOMMQa9/56vSS1mNmDEp4stmtRsHF0Vr5q56HXajhvVOUmdXAYL4eXG4ndlhP
+# gGjQP8aCa5NQQjHPD+2dXohSoRZskgkrGo6J1aUy6HVak7q+csmlo1Q81Qej/bGe
+# U1hqoN7EXlh619HF+6YzyIFLDxDRZ2l0bhclTBBcI++Kr85rrWO/awFeZCYAU7cW
+# G/YPTDVGRdOP6R/LPkN+HnadVigFMkFIVoR9twXI5C4LAq4B8RRH+eU8c7iHcCcd
+# 9+gGTkeFWSXvrmEgvyGtZsZMhWtEWbM47EVfAUGhMpVw2fm6j8sxggHiMIIB3gIB
+# ATAwMBwxGjAYBgNVBAMMEU15Q29kZVNpZ25pbmdDZXJ0AhBWuR3GMKENtk9swczn
+# Hdg7MA0GCWCGSAFlAwQCAQUAoIGEMBgGCisGAQQBgjcCAQwxCjAIoAKAAKECgAAw
+# GQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYBBAGCNwIBCzEOMAwGCisG
+# AQQBgjcCARUwLwYJKoZIhvcNAQkEMSIEIKoB2DWTHkzMlpUb3EwvjsPvP98sCuHT
+# WkFD3VjmmJBqMA0GCSqGSIb3DQEBAQUABIIBADfUCdIqEVVRT5kt6KnnZrDSM7c/
+# HfKa3hPA2Nc7PAIdlfL8OSuVa8gFiauM+EdGbLD4cfOxzkBoVKD+45qy9XfdklX5
+# psM9sc69UCcEm1s3AnMToz4+/y4aJQhg/7cIW64X9kb87Y3Y9gUd82oPDtLIB3sr
+# 7KUl0BpEOa4SQc/2nfrIecKVni9/gm1fFYImsKJ6ngvJ2mFFvmB2PxJnxOYhErym
+# smlWWk4I8J+dZYKHmDJiJt42ZwuqKOL5axrPFd23Wsm8ZY6YYKEiECchpBOSW35w
+# k4A4GCRZPb3gl1+8zifnS1dKchNkAHa178cyyeN9xW0XEA67+fhxqTnZd8U=
+# SIG # End signature block
